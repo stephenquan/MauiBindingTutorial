ecdf4497eb85c656a63a24b07e0601d34e52687e
Stephen Quan <squan@esri.com>
2025-05-15 17:44:16 +1000

    Shorten Primes initializer

--- MauiBindingTutorial/MainPage.xaml.cs
+++ MauiBindingTutorial/MainPage.xaml.cs
@@ -1,7 +1,7 @@
 ﻿namespace MauiBindingTutorial;
 public partial class MainPage : ContentPage
 {
-    public List<int> Primes { get; } = new List<int>() { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+    public List<int> Primes { get; } = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
     public MainPage()
     {
         InitializeComponent();
